146fe8c6ad1689426b2f23989efe196c2424ec5d
ofcpire <ofcpire@gmail.com>
2024-05-30 03:50:28 +0900

    Feat: jwt토큰 인터셉터

---
+++ src/lib/interceptor/auth.strategy.ts
@@ -0,0 +1,32 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { AuthService } from 'src/service/members/auth.service';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AuthStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly authService: AuthService,
+    private configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: (req) => {
+        let token = ExtractJwt.fromAuthHeaderAsBearerToken()(req);
+        if (!token && req.headers.refresh) {
+          token = req.headers.refresh;
+        }
+        return token;
+      },
+      secretOrKey: configService.get('AUTH_SECRET'),
+    });
+  }
+
+  async validate(payload: { email: string }) {
+    const member = await this.authService.getMemberInfoByEmail(payload.email);
+    if (!member) {
+      throw new UnauthorizedException();
+    }
+    return member;
+  }
+}

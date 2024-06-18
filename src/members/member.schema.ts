ffbc6e7858dfe999e80a14792791839ed4047f0f
ofcpire <ofcpire@gmail.com>
2024-06-19 04:28:55 +0900

    Fix: 멤버 스키마 닉네임 설정

--- src/members/member.schema.ts
+++ src/members/member.schema.ts
@@ -15,7 +15,7 @@ export class Member extends Document {
   @Prop({ required: true, unique: true })
   email: string;
 
-  @Prop({ required: true, default: '차몽인' })
+  @Prop({ required: true, unique: true })
   nickname: string;
 
   @Prop({ required: true })

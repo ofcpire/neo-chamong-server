76ac7adde3a606e55db6324dabb3ca1b51616d08
ofcpire <ofcpire@gmail.com>
2024-06-28 01:21:31 +0900

    Fix: logger 코드 변경

--- src/common/interceptor/logging-interceptor.ts
+++ src/common/interceptor/logging-interceptor.ts
@@ -3,21 +3,17 @@ import {
   NestInterceptor,
   ExecutionContext,
   CallHandler,
+  Logger,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
+  private readonly logger = new Logger(LoggingInterceptor.name);
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request = context.switchToHttp().getRequest();
-    const method = request.method;
-    const url = request.url;
-    const headers = request.headers;
-
-    console.log(`Req Method: ${method}, URL: ${url}`);
-    console.log(headers);
-
+    this.logger.log(`${request.method} ${request.url}`);
     const now = Date.now();
     return next
       .handle()

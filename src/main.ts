3ef5bd9a1b5ad831c4eb2ff9329b7aea2e8a90bc
ofcpire <ofcpire@gmail.com>
2024-06-28 19:44:58 +0900

    Squashed commit of the following:
    commit 63ac7a7150a2fd096753cb98b1b70caee0baa1fd
    Author: ofcpire <ofcpire@gmail.com>
    Date:   Fri Jun 28 17:47:15 2024 +0900
    
        Fix: helmet 옵션 제거
    
    commit 76ac7adde3a606e55db6324dabb3ca1b51616d08
    Author: ofcpire <ofcpire@gmail.com>
    Date:   Fri Jun 28 01:21:31 2024 +0900
    
        Fix: logger 코드 변경
    
    commit 3dbc2fa3c9b591dc9698562a392063a4aa997a9b
    Author: ofcpire <ofcpire@gmail.com>
    Date:   Sat Jun 22 23:48:45 2024 +0900
    
        Feat: dev logging interceptor
    
    commit c2eed0d2cf47ab52b8f1be7c64bef191ff4486d1
    Author: ofcpire <ofcpire@gmail.com>
    Date:   Sat Jun 22 23:39:46 2024 +0900
    
        Chore: 기본 보안 설정

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,22 @@ import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
-
-const corsOption: CorsOptions = {
-  exposedHeaders: ['Authorization', 'Refresh'],
-};
+import helmet from 'helmet';
+import { LoggingInterceptor } from './common/interceptor/logging-interceptor';
 
 async function bootstrap() {
   dotenv.config();
+  const origin = process.env.ORIGIN || '*';
+  const port = process.env.PORT || 15941;
+  const corsOption: CorsOptions = {
+    exposedHeaders: ['Authorization', 'Refresh'],
+    origin: origin,
+  };
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors(corsOption);
-  await app.listen(15941);
+  app.useGlobalInterceptors(new LoggingInterceptor());
+  await app.listen(port);
 }
 bootstrap();
